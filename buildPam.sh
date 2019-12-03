c039a5285aa6c3fe6d4e4ce58f185e236da2e570
Shawn <baxcky@gmail.com>
2019-12-03 09:35:40 +0800

    Update buildPam.sh
    Update path with ubuntu 14.04

--- buildPam.sh
+++ buildPam.sh
@@ -2,6 +2,7 @@
 
 gcc -fPIC -fno-stack-protector -c src/mypam.c
 
-sudo ld -x --shared -o /lib/security/mypam.so mypam.o
-
+#sudo ld -x --shared -o /lib/security/mypam.so mypam.o
+# ubuntu 14.04
+sudo ld -x --shared -o /lib/x86_64-linux-gnu/security/mypam.so mypam.o
 rm mypam.o
